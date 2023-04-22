d302ab45f6385a072fad6068fd2908d78aae9913
Mauricio <maurolores1992@gmail.com>
2023-04-22 03:20:25 -0300

    terminando cambios

--- php/asistenciaCeremonia.php
+++ php/asistenciaCeremonia.php
@@ -3,8 +3,8 @@
   $subject = "Confirmacion Asistencia";
   $nombreyapellido = $_POST['nombreyapellido'];
   $codigo = $_POST['codigo'];
-  $datoimportante = $_POST['datoimportante'];
-  $headers = "Nombre y Apellido: ".$nombreyapellido . "\n" . "Codigo de confirmacion: " . $codigo . "\n" . "Dato importante: " . $datoimportante;
+  $datoImportante = $_POST['datoImportante'];
+  $headers = "Nombre y Apellido: ".$nombreyapellido . "\n" . "Codigo de confirmacion: " . $codigo . "\n" . "Dato importante: " . $datoImportante;
 
   if(mail($to_email, $subject, $headers)) {
     echo '<script language="javascript">
