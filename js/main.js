71b953104d705325b614ed2d924bb55d930abdd0
Mauricio <maurolores1992@gmail.com>
2023-04-25 01:09:42 -0300

    portada

--- js/main.js
+++ js/main.js
@@ -148,7 +148,7 @@ function showSlides() {
       slideIndex = 1
   }
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides,6000);
+  setTimeout(showSlides,4000);
 }
 showSlides();
 
